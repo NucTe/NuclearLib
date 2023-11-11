c4408ac6fea49e705b34b055f6cc614e7d6c22d2
1mag1n33 <kopeccarson@gmail.com>
2023-11-10 21:12:35 -0500

    Added Registery

--- src/main/java/me/nuclearteam/nuclearlib/Nuclearlib.java
+++ src/main/java/me/nuclearteam/nuclearlib/Nuclearlib.java
@@ -1,22 +1,37 @@
 package me.nuclearteam.nuclearlib;
 
+import me.nuclearteam.nuclearlib.Register.PluginRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Nuclearlib extends JavaPlugin {
 
     private static Nuclearlib instance;
+    private PluginRegistry pluginRegistry;
 
     @Override
     public void onEnable() {
         instance = this;
+        pluginRegistry = new PluginRegistry();
+
+        // Register this library as a plugin
+        pluginRegistry.registerPlugin(this);
+
+        // Other initialization logic
     }
 
     @Override
     public void onDisable() {
+        // Unregister this library as a plugin
+        pluginRegistry.unregisterPlugin(this);
+
+        // Other library shutdown logic
     }
 
     public static Nuclearlib getInstance() {
         return instance;
     }
 
+    public PluginRegistry getPluginRegistry() {
+        return pluginRegistry;
+    }
 }
